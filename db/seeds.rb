08b95d40b269fc0b03a137e0a69c5d99e3aa6a59
Xelaflash <xelaflash@gmail.com>
2018-05-22 10:33:17 -0400

    devise set up

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ puts "destroy all"
 
 puts "start seed"
 
-u = User.create!(email: "toto@toto.com", password: "azerty", admin: true, prenom: "toto", nom: "toto", adresse: "21 rue des champs 75005 paris", telephone: "0600000000" )
+User.create!(email: "toto@toto.com", password: "azerty", admin: true, prenom: "toto", nom: "toto", adresse: "21 rue des champs 75005 paris", telephone: "0600000000")
 
 
 puts "end seed"
