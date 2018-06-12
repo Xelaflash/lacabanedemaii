6a4f622fd77caeed15a1946fe69234bb6b4c6d9a
Xelaflash <xelaflash@gmail.com>
2018-06-12 10:10:06 -0400

    cart ajax ok

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -24,8 +24,7 @@ class OrderItemsController < ApplicationController
     @order_item = @order.order_items.find(params[:id])
     @order_item.update_attributes(order_item_params)
     @order_items = @order.order_items
-    redirect_to cart_path
-    reload
+
   end
 
   def destroy
@@ -33,7 +32,6 @@ class OrderItemsController < ApplicationController
     @order_item = @order.order_items.find(params[:id])
     @order_item.destroy
     @order_items = @order.order_items
-    redirect_to cart_path
   end
 
 private
