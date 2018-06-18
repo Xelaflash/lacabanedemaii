93faadeecf2a7662fdad21f62dd6ed109c39f4a7
Xelaflash <xelaflash@gmail.com>
2018-06-18 11:38:35 -0400

    js only working for one item to review

--- app/javascript/components/form.js
+++ app/javascript/components/form.js
@@ -1,14 +1,13 @@
-  document.addEventListener('click', (event) => {
-      document.querySelector('select[name="order_item[quantity]"]').onchange=changeEventHandler;
-    });
+// document.addEventListener('DOMContentLoaded',function() {
+//     document.querySelector('select[name="order_item[quantity]"]').onchange=updateQuantity;
+// },false);
 
-      function changeEventHandler(event2) {
-        const newQuantity = event2.target.value;
-        alert('You have change the quantity to ' + event.target.value);
-        console.log(newQuantity);
+//       function updateQuantity(event2) {
+//         const newQuantity = event2.target.value;
+//         console.log(newQuantity);
+//         alert('You have change the quantity to ' + event.target.value);
 
-        const submit = document.getElementById('submit-form');
-        submit.click();
-      };
-
+//         const submit = document.getElementById('submit-form');
+//         submit.click();
+// };
 
