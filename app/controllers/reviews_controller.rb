bfc9037408a1da3a5067aef93f02127e5184d461
Xelaflash <xelaflash@gmail.com>
2018-06-29 12:22:32 -0400

    avg styling

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,7 +10,6 @@ class ReviewsController < ApplicationController
       redirect_to produit_path(@produit)
     else
       render "produits/show"
-      # flash[:alert] = "Le commentaire n'a pas pu être ajouté. Merci de réessayer"
     end
   end
 
--- app/models/review.rb
+++ app/models/review.rb
@@ -4,5 +4,6 @@ class Review < ApplicationRecord
 
   validates :content, presence: true
   validates :rating, presence: true, inclusion: { in: [1, 2, 3, 4, 5] }
+  validates :author, presence: true, uniqueness: true
 
 end
