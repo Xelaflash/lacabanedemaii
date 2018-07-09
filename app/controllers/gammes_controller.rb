a7909495a5f9222943247dadd3ff72230c4b7b24
Xelaflash <xelaflash@gmail.com>
2018-07-09 11:51:37 -0400

    tab product display working + add tab in index + show page

--- app/controllers/gammes_controller.rb
+++ app/controllers/gammes_controller.rb
@@ -7,6 +7,7 @@ class GammesController < ApplicationController
   end
 
   def show
+    @gammes = Gamme.all
     @gamme = Gamme.find(params[:id])
     @produits = Produit.where(gamme_id: @gamme.id)
     @order_item = current_order.order_items.new
