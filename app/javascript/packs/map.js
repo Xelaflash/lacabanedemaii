931b62b46c103a767f6026982065dd9332f33ea3
Xelaflash <xelaflash@gmail.com>
2018-07-20 17:19:04 -0400

    g set up

---
+++ app/javascript/packs/map.js
@@ -0,0 +1,13 @@
+import GMaps from 'gmaps/gmaps.js';
+
+const mapElement = document.getElementById('map'); {
+  var store = {lat: 44.741486, lng: -1.09817};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: store
+    });
+  var marker = new google.maps.Marker({
+    position: store,
+    map: map
+  });
+}
