55a00ab92e83e486515a1c04d6733922e1318371
Xelaflash <xelaflash@gmail.com>
2018-07-27 10:17:56 -0400

    cleaning comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'contact/new'
-  get 'contact/create'
   ActiveAdmin.routes(self)
   devise_for :users
 
@@ -13,11 +11,10 @@ Rails.application.routes.draw do
   resources :gammes, only: [:show]
   resources :contacts, only: [:new, :create]
 
-  # get 'contact', to: 'contacts#new', as: 'new_contact'
-  # post 'contact', to: 'contacts#create', as: 'create_contact'
-
   root to: 'pages#home'
 
+  get 'contact/new'
+  get 'contact/create'
   get 'qui_sommes_nous', to: 'pages#qui_sommes_nous', as: :qui_sommes_nous
   get 'livraison',       to: 'pages#livraison',       as: :livraison
   get 'aide',            to: 'pages#aide',             as: :aide
