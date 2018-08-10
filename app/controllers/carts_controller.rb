0fa4a4608fa882cfc80d9ab6e2c1ae899eeeb1a7
Xelaflash <xelaflash@gmail.com>
2018-08-10 12:56:16 -0400

    breadcrumbs cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -5,6 +5,7 @@ class CartsController < ApplicationController
     @order_items = current_order.order_items
     @order = current_order
     add_breadcrumb "accueil", :root_path
+    add_breadcrumb "produits", produits_path
     add_breadcrumb "panier", cart_path
   end
 end
