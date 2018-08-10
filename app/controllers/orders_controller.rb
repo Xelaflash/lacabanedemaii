f51c3aabe1afff76668d35b392da2cd81c241081
Xelaflash <xelaflash@gmail.com>
2018-08-10 11:23:39 -0400

    order index

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,13 +4,22 @@ class OrdersController < ApplicationController
     @order =  current_order
     @order.user_id = current_user.id
     order_item = @order.order_items
+    @order.save
     redirect_to new_order_payment_path(@order)
+  end
+
+  def index
+    @orders = current_user.orders.all
   end
 
   def show
     @order =  current_order
     @order.user_id = current_user.id
     @order_items = @order.order_items
+  end
+
+  def destroy
+
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :reviews
-  has_many :orders
+  has_many :orders, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true
   validates :nom, presence: true
