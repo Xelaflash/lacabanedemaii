8797d4c702f4f26aa08a86bf69f785b288fa9cc3
Xelaflash <xelaflash@gmail.com>
2018-08-23 14:06:52 -0400

    review contact form

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,8 @@
 class Contact
   include ActiveModel::Model
-  attr_accessor :name, :email,:phone_number, :body
+  attr_accessor :name, :email,:subject, :body
   validates :name, presence: true
   validates :email, presence: true
-  validates :phone_number, presence: true
+  validates :subject, presence: true
   validates :body, presence: true
 end
