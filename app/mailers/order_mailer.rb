ee1d9885c2f6b8cb11c0457f5c53b4ef39cbe504
Xelaflash <xelaflash@gmail.com>
2018-08-24 16:34:01 -0400

    mail body ok footer to done

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,6 +2,7 @@ class OrderMailer < ApplicationMailer
 
   def order_confirmation_user(order)
     @order_pay = order
+    @order_items = order.order_items
     mail(
       to:       @order_pay.customer,
       subject:  "Confirmation de la commande n° #{@order_pay.id}"
