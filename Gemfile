eb8c5b4f2e7ffcd0a739f8de5b2f5fbf8640229c
Xelaflash <xelaflash@gmail.com>
2018-08-24 11:26:26 -0400

    set upmailing

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem "breadcrumbs_on_rails"
 
 group :development do
   gem 'web-console', '>= 3.3.0'
+  gem "letter_opener"
 end
 
 group :development, :test do
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,9 @@ class ContactMailer < ApplicationMailer
 
   def contact_me(message)
       @body = message.body
-      mail to: "lacabanedemaii.website@gmail.com", from: message.email
+      @sender = message.email
+      @subject = message.subject
+
+      mail (to: 'lacabanedemaii@gmail.com', from: @sender, subject: @subject)
   end
 end
