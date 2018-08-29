240696346070e8a0ab2dfd2d6c1dc58e79c2dcf3
Xelaflash <xelaflash@gmail.com>
2018-08-29 08:52:26 -0400

    mailer review after testing

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,15 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'contact@lacabanedemaii.com'
   layout 'mailer'
-
-  class TestMailer < ActionMailer::Base
-    def message
-      mail(
-        :subject => 'Hello from Postmark',
-        :to  => 'contact@lacabanedemaii.com',
-        :from => 'contact@lacabanedemaii.com',
-        :html_body => '<strong>Hello</strong> dear Postmark user.',
-        :track_opens => 'true')
-    end
-  end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,6 +8,6 @@ class UserMailer < ApplicationMailer
   def welcome(user)
     @user = user
     @url  = 'https://lacabanedemaii.herokuapp.com/users/sign_in'
-    mail(to: @user.email, subject: 'Bienvenu à la Cabane de Maii')
+    mail(to: @user.email, subject: 'Bienvenu à la cabane de Maii')
   end
 end
