8a304730d7e22ed398f4e63ad3c2025661a81828
Xelaflash <xelaflash@gmail.com>
2018-09-11 07:53:20 -0400

    review job

--- app/jobs/delete_old_empty_orders_job.rb
+++ app/jobs/delete_old_empty_orders_job.rb
@@ -1,10 +1,9 @@
 class DeleteOldEmptyOrdersJob < ApplicationJob
   queue_as :empty_orders
 
-  def perform(order_id)
-    order_to_kill_list = Order.where(prod_list: nil, user_id: nil,).where("created_at < ?", 2.days.ago).map
+  def perform(_order_id)
+    order_to_kill_list = Order.where(prod_list: nil, user_id: nil)
+                              .where('created_at < ?', 1.day.ago).map
     Order.where(id: order_to_kill_list.map(&:id)).destroy_all
   end
 end
-
-
