ce349da86dd94c9266f6292d279a6b831eb93b09
Xelaflash <xelaflash@gmail.com>
2018-09-11 08:13:10 -0400

    review timing job

--- app/jobs/delete_old_empty_orders_job.rb
+++ app/jobs/delete_old_empty_orders_job.rb
@@ -3,7 +3,7 @@ class DeleteOldEmptyOrdersJob < ApplicationJob
 
   def perform(_order_id)
     order_to_kill_list = Order.where(prod_list: nil, user_id: nil)
-                              .where('created_at < ?', 1.day.ago).map
+                              .where('created_at < ?', 12.hours.ago).map
     Order.where(id: order_to_kill_list.map(&:id)).destroy_all
   end
 end
