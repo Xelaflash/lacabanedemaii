7d54e52adcd4a0c87d4b89af5ab0e65fb321e14f
Xelaflash <xelaflash@gmail.com>
2019-04-04 08:41:27 -0400

    sidenav ok when user signin/signout

--- app/javascript/components/sidenav.js
+++ app/javascript/components/sidenav.js
@@ -1,12 +1,12 @@
-const navBtn = document.getElementById("burger");
-const closeBtn = document.getElementById("closebtn");
+const navBtn = document.querySelector(".burger");
+const closeBtn = document.querySelector(".closebtn");
 
 function openNav() {
-  document.getElementById("mySidenav").style.width = "100%";
+  document.querySelector(".sidenav").style.width = "100%";
 }
 
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+  document.querySelector(".sidenav").style.width = "0";
 }
 
 navBtn.addEventListener("click", openNav);
