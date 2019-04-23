9e185aae8d50334a6a877caf7497a1e9db1fd6e9
Xelaflash <xelaflash@gmail.com>
2019-04-23 09:49:40 -0400

    review ajax working, fix for modal to do

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   end
 
   resources :produits, only: [:index, :show] do
-    resources :reviews, only: [ :create ]
+    resources :reviews, only: :create
   end
   resource :cart, only: [:show]
   resources :order_items, only: [:create, :update, :destroy]
