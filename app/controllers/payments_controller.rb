0bde56b82fdbc1238160714578d3a77065dbb0c9
Xelaflash <xelaflash@gmail.com>
2019-04-25 10:33:53 -0400

    stripe v2 to review not working

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -9,20 +9,31 @@ class PaymentsController < ApplicationController
   end
 
   def create
-    Stripe.api_version = '2018-11-08; checkout_sessions_beta=v1'
     customer = Stripe::Customer.create(
       source: params[:stripeToken],
       email:  params[:stripeEmail]
     )
-    # charge = Stripe::Charge.create(
-    #   customer:     customer.id,   # You should store this customer id and re-use it.
-    #   amount:       @order_pay.total_price_cents,
-    #   description:  "Paiement pour la commande n° #{@order_pay.id} du #{@order_pay.created_at} d'un montant de #{@order_pay.total_price} €",
-    #   currency:     'eur'
-    # )
-
-
+    charge = Stripe::Charge.create(
+      customer:     customer.id,   # You should store this customer id and re-use it.
+      amount:       @order_pay.total_price_cents,
+      description:  "Paiement pour la commande n° #{@order_pay.id} du #{@order_pay.created_at} d'un montant de #{@order_pay.total_price} €",
+      currency:     'eur'
+    )
     # TODO: New stripe checkout!!!
+    # session = Stripe::Checkout::Session.create(
+    # success_url: 'https://www.example.com/success',
+    # cancel_url: 'https://www.example.com/cancel',
+    # payment_method_types: ['card'],
+    # line_items: @order_pay.order_items.map do |item|
+    #   {
+    #     amount: item.total_price_cents,
+    #     quantity: item.quantity,
+    #     name: item.name,
+    #     currency: 'eur'
+    #   }
+    # end
+    # )
+    # [200, { session: session.id }.to_json]
 
     flash[:notice] = "Votre paiement a été accepté. Vous allez recevoir un mail de confirmation."
     @order_pay.update(payment: charge.to_json, order_status_id: 2, active: false)
@@ -30,6 +41,8 @@ class PaymentsController < ApplicationController
     # OrderShopMailer.order_confirmation_shop(@order_pay).deliver_later(wait: 2.minutes)
     OrderMailer.order_confirmation_user(@order_pay).deliver_now
     OrderShopMailer.order_confirmation_shop(@order_pay).deliver_now
+
+
     @order_last = current_user.orders.last
     redirect_to order_path(@order_last)
 
---
+++ app/javascript/components/stripe.js
@@ -0,0 +1,5 @@
+// var stripe = Stripe('pk_test_qvXSpLJZH3qVpz6UATEv1bt3');
+
+// stripe.redirectToCheckout({
+//   sessionId: '{{CHECKOUT_SESSION_ID}}'
+// });
