50c5adf262809e673b9dea8da6051975f2c5f69a
Xelaflash <xelaflash@gmail.com>
2019-09-16 12:26:47 -0400

    remove useless code

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,14 +1,3 @@
-// import GMaps from 'gmaps/gmaps.js';
-
-// const mapElement = document.getElementById('map');
-
-// if (mapElement) {
-//   const store = { lat: 44.741632, lng: -1.098127 };
-//   const map = new GMaps({ el: '#map', zoom: 15, center: store });
-//   map.addMarker({ lat: store.lat, lng: store.lng });
-// }
-
-
 const mapElement = document.getElementById('map');
 
 if (mapElement) {
@@ -17,8 +6,9 @@ if (mapElement) {
     zoom: 15,
     center: store
   });
+  // eslint-disable-next-line no-unused-vars
   const marker = new google.maps.Marker({
     position: store,
-    map: map
+    map
   });
 }
